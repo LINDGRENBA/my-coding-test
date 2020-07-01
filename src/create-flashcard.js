d79c13fedec1a7e0a28e8097e5465e0ac5ef057c
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-30 20:16:43 -0700

    Achieve pass on second test to create Flashcard object, update README.md to reflect specification met.

--- src/create-flashcard.js
+++ src/create-flashcard.js
@@ -1,9 +1,10 @@
 export class Flashcard {
-  constructor() {
-
+  constructor(question, possibleAnswers, correctAnswer) {
+    this.question = question;
+    this.possibleAnswers = possibleAnswers;
+    this.correctAnswer = correctAnswer;
+    this.timesUp = 30;
   }
 }
 
-
-// |    Program will create a flashcard object with the question, an array of possible answers, correct answer and timesUp     |    n/a    |    flascard{question: "What's javascript?", possAnswers = ["A city in North Dakota", "An new species of animal", correctAnswer: "A programming language", timesUp: 30], "A programming language"}    |     N     |
 
